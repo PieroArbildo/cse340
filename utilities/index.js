572d861d47fac185ef7a17599e7af81650820856
PieroArbildo <89119204+PieroArbildo@users.noreply.github.com>
2024-10-18 22:05:27 -0500

    mvc Implementation

--- utilities/index.js
+++ utilities/index.js
@@ -25,7 +25,7 @@ Util.getNav = async function (req, res, next) {
   return list
 }
 
-module.exports = Util
+
 
 
 
@@ -61,3 +61,16 @@ Util.buildClassificationGrid = async function(data){
   }
   return grid
 }
+
+
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+
+
+module.exports = Util
